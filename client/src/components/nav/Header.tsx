b53d7c69e0a2510c3fcea034b145036694626b91
kbzcraft <ytkbzcraft@gmail.com>
2024-09-05 18:28:58 +0545

    blurbg size&pos

--- client/src/components/nav/Header.tsx
+++ client/src/components/nav/Header.tsx
@@ -18,7 +18,9 @@ const Header = ({
     <header className="flex  text-s1 w-full border border-transparent border-b-line">
       <div className="flex md:pr-20 px-6 gap-2 items-center">
         <MenuFill className="h-fit md:hidden" />
-        <h1 className="w-full max-w-44  py-2 cursor-default">{name}</h1>
+        <Link href="/" className="w-full max-w-44  py-2 cursor-default">
+          {name}
+        </Link>
       </div>
       <nav className="grow flex sm:justify-between  select-none">
         <ul className="sm:flex hidden">
