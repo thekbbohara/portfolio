f24f18c30ff6f64b8675272896dc6c28f85b7194
kbzcraft <ytkbzcraft@gmail.com>
2024-09-07 17:52:08 +0545

    key proop

--- client/src/components/project/ProjectContainer.tsx
+++ client/src/components/project/ProjectContainer.tsx
@@ -16,7 +16,7 @@ const ProjectContainer = ({
   return (
     <section className="w-full grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-8 p-4 sm:p-8">
       {projects.map(({ projectName, title, img, href, alt }, id) => (
-        <div className="flex flex-col h-[314px] mx-auto">
+        <div key={id} className="flex flex-col h-[314px] mx-auto">
           <h1 className="flex gap-2 py-3">
             <strong className="text-s3">Project {id + 1}</strong>
             <span>{`//_${projectName}`}</span>
