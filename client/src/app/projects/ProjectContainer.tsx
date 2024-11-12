021c4c58b6913a54270a6e339c3314dd33d4ce26
kbzcraft <ytkbzcraft@gmail.com>
2024-11-12 09:01:37 +0545

    latest project first

--- client/src/app/projects/ProjectContainer.tsx
+++ client/src/app/projects/ProjectContainer.tsx
@@ -15,12 +15,12 @@ interface Iproject {
 
 const projectData: Iproject[] = [
   {
-    projectName: "spfyui",
-    title: "cli tool to manage react icons and components.",
-    img: "/assets/spfyui.png",
-    href: "https://www.npmjs.com/package/spfyui",
-    alt: "spfyui",
-    tags: ["nodejs"],
+    projectName: "assign-sync",
+    title: "Realtime code assignment /sɪŋkrənʌɪˈzeɪʃ(ə)n/ platform.",
+    img: "/assets/assign-sync.png",
+    href: "https://assign-sync.vercel.app/",
+    alt: "assign-sync",
+    tags: ["web-socket", "nextjs", "bun"],
   },
   {
     projectName: "ai-calculator",
@@ -29,15 +29,15 @@ const projectData: Iproject[] = [
     href: "https://ai-calculator-rose.vercel.app/",
     alt: "ai-calculator[rose]",
     tags: ["nextjs", "typescript"],
-  },
-  {
-    projectName: "assign-sync",
-    title: "Realtime code assignment /sɪŋkrənʌɪˈzeɪʃ(ə)n/ platform.",
-    img: "/assets/assign-sync.png",
-    href: "https://assign-sync.vercel.app/",
-    alt: "assign-sync",
-    tags: ["web-socket", "nextjs", "bun"],
-  },
+  }, {
+    projectName: "spfyui",
+    title: "cli tool to manage react icons and components.",
+    img: "/assets/spfyui.png",
+    href: "https://www.npmjs.com/package/spfyui",
+    alt: "spfyui",
+    tags: ["nodejs"],
+  }
+  ,
 ];
 
 export default function ProjectContainer() {
