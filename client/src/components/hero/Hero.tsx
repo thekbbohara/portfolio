d9b6213a7d843a757dc4ce571496fa4666a8096c
thekbbohara <ytkbzcraft@gmail.com>
2024-12-11 22:36:50 +0545

    added activity to hero

--- client/src/components/hero/Hero.tsx
+++ client/src/components/hero/Hero.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroContent from "./HeroContent";
+import { ActivityCard } from "./activity";
 
 const Hero = () => {
   return (
-    <section className="flex sm:m-0  sm:pl-32 md:pl-48 m-auto justify-between items-center">
+    <section className="flex sm:m-0 flex-wrap  sm:pl-32 md:pl-48 m-auto gap-2 items-center">
       <HeroContent />
+      <ActivityCard className="grow mx-auto" />
     </section>
   );
 };
