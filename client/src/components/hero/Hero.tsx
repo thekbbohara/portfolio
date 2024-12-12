9a38470a93f25f43a8c5e9d932ac60b3aa35fd29
thekbbohara <ytkbzcraft@gmail.com>
2024-12-12 15:38:22 +0545

    res stuff

--- client/src/components/hero/Hero.tsx
+++ client/src/components/hero/Hero.tsx
@@ -4,9 +4,9 @@ import { ActivityCard } from "./activity";
 
 const Hero = () => {
   return (
-    <section className="flex sm:m-0 flex-wrap  sm:pl-32 md:pl-48 m-auto gap-2 items-center">
+    <section className="flex max-h-screen flex-col md:flex-row  m-auto md:m-0 sm:px-28 md:px-48 gap-12 md:gap-2 md:items-center">
       <HeroContent />
-      <ActivityCard className="grow mx-auto" />
+      <ActivityCard className="grow md:ml-auto  flex flex-col gap-4 sm:gap-4 sm:px-0 sm:mx-0 mx-auto" />
     </section>
   );
 };
