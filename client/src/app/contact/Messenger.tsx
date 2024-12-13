24b8cad97cc8ceb25093fdd53a2f5ded7750bba1
thekbbohara <ytkbzcraft@gmail.com>
2024-12-13 23:50:45 +0545

    ui

--- client/src/app/contact/Messenger.tsx
+++ client/src/app/contact/Messenger.tsx
@@ -1,14 +1,20 @@
-'use client'
+"use client";
 
 import Button from "@/components/ui/Buttons";
 import cn from "@/utils/cn";
 import Image from "next/image";
-import React, { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import UsrInfoDialog from "./usrInfoDialog";
 import MessengerHeader from "./Messenger/header";
 import { useMessengerContext } from "./Messenger/provider";
 import MessengerFooter from "./Messenger/footer";
-
+import { isArray } from "util";
 
 export interface IMessenger {
   msg: string;
@@ -19,22 +25,20 @@ export interface IMessenger {
   setEmail: Dispatch<SetStateAction<string>>;
 }
 
-
-
 const Messenger = () => {
-  const { messages, dialogRef } = useMessengerContext()
-  const [maxMsgBoxHeight, setMaxMsgBoxHeight] = useState<number>(0)
-  const msgBoxRef = useRef<null | HTMLDivElement>(null)
+  const { messages, dialogRef } = useMessengerContext();
+  const [maxMsgBoxHeight, setMaxMsgBoxHeight] = useState<number>(0);
+  const msgBoxRef = useRef<null | HTMLDivElement>(null);
   useEffect(() => {
     if (!msgBoxRef.current) return;
     const maxHeight = msgBoxRef.current?.clientHeight;
-    setMaxMsgBoxHeight(maxHeight || 0)
+    setMaxMsgBoxHeight(maxHeight || 0);
     msgBoxRef.current.style.maxHeight = `${maxHeight}px`;
-  }, [])
+  }, []);
   useEffect(() => {
     if (!msgBoxRef.current) return;
     msgBoxRef.current.scrollTop = msgBoxRef.current.scrollHeight;
-  }, [messages])
+  }, [messages]);
   return (
     <section className="w-full max-w-[98vw] mx-auto sm:mx-0  border border-transparent border-l-line sm:border-l-transparent  border-r-line p-4 flex flex-col justify-between">
       <UsrInfoDialog ref={dialogRef} />
@@ -44,8 +48,9 @@ const Messenger = () => {
         ref={msgBoxRef}
         className={cn(
           `grow flex flex-col overflow-y-scroll [&::-webkit-scrollbar]:w-2`,
-          { [`max-h-[${maxMsgBoxHeight}px]`]: maxMsgBoxHeight }
-        )}>
+          { [`max-h-[${maxMsgBoxHeight}px]`]: maxMsgBoxHeight },
+        )}
+      >
         <div
           id="info-div"
           className="flex flex-col items-center mt-auto leading-9 mb-6"
@@ -67,28 +72,43 @@ const Messenger = () => {
           </Button>
         </div>
         <div className="flex flex-col gap-2 pb-1 ">
-          {messages.map((message, id) => <>
-            {id == 0 &&
-              <span className="mx-auto">
-                {message.date}
-              </span>}
-            {id >= 1 && message.date != message.date &&
-              <span className="mx-auto">
-                {message.date}
-                {message.time}
-              </span>}
-            <div className={cn("leading-5 rounded-2xl max-w-[70%]  py-2 px-3 flex gap-2", message.sender === "admin" ? "mr-auto bg-s2" : "ml-auto bg-s3")}>
-              <span
-                key={id}
-                className=" text-s4 "
+          {messages.map((message, id) => (
+            <>
+              {id == 0 && <span className="mx-auto">{message.date}</span>}
+              {id >= 1 &&
+                message.msg.toString() != "null" &&
+                message.date != message.date && (
+                  <span className="mx-auto">
+                    {message.date}
+                    {message.time}
+                  </span>
+                )}
+              <div
+                className={cn(
+                  "leading-5 rounded-2xl max-w-[70%]  py-2 px-3 flex gap-2",
+                  message.sender === "admin"
+                    ? "mr-auto bg-s2"
+                    : "ml-auto bg-s3",
+                )}
               >
-                {message.msg}
-              </span>
-              <span className="text-p3 min-w-fit text-xs opacity-70 mt-auto tracking-[-0.09rem]">
-                {message.time}
-              </span>
-            </div>
-          </>)}
+                {isArray(message.msg)
+                  ? message.msg.map((reply, i) => (
+                      <span key={i} className=" text-s4 ">
+                        {reply}
+                      </span>
+                    ))
+                  : message.msg.toString() != "null" && (
+                      <span key={id} className=" text-s4 ">
+                        {message.msg}
+                      </span>
+                    )}
+
+                <span className="text-p3 min-w-fit text-xs opacity-70 mt-auto tracking-[-0.09rem]">
+                  {message.time}
+                </span>
+              </div>
+            </>
+          ))}
         </div>
       </div>
       <MessengerFooter />
