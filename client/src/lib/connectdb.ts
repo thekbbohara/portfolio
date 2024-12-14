f107af949d8302b3ecc122f9455ef1d19e976fc8
thekbbohara <ytkbzcraft@gmail.com>
2024-12-14 20:32:39 +0545

    fmt

--- client/src/lib/connectdb.ts
+++ client/src/lib/connectdb.ts
@@ -1,6 +1,6 @@
-import { connect, type Mongoose } from "mongoose"
+import { connect, type Mongoose } from "mongoose";
 
-const uri = process.env.SINGULAR_CHAT_MONGODB_URI
+const uri = process.env.SINGULAR_CHAT_MONGODB_URI;
 
 if (!uri) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
@@ -13,7 +13,7 @@ declare global {
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 const dbConnect = async () => {
@@ -23,16 +23,16 @@ const dbConnect = async () => {
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
-    }
+    };
     try {
-      cached.promise = connect(uri, opts)
-      cached.conn = await cached.promise
+      cached.promise = connect(uri, opts);
+      cached.conn = await cached.promise;
     } catch (e) {
       cached.promise = null;
       throw e;
     }
   }
-  return cached.conn
-}
+  return cached.conn;
+};
 
-export default dbConnect
+export default dbConnect;
