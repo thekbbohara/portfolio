060576e154898369075f9f68db0778bb886e0bb8
thekbbohara <ytkbzcraft@gmail.com>
2024-12-14 20:33:38 +0545

    chats schema

--- client/src/model/user.ts
+++ client/src/model/user.ts
@@ -1,18 +1,30 @@
-import { Schema, model, models } from "mongoose";
+import { msgProps } from "@/app/contact/Messenger/provider";
+import { ObjectId, Schema, model, models } from "mongoose";
 
 export interface IUser {
+  _id?: any;
   name: string;
   email: string;
-  chats: { text: string }[];
+  chats: msgProps[];
 }
 
-const userSchema = new Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  chats: { type: [{ text: String }] }
-}, {
-  timestamps: true
-});
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    chats: {
+      type: [
+        {
+          sender: { type: String, enum: ["user", "admin"], required: true },
+          msg: { type: String, default: null },
+        },
+      ],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const UserModel = models.UserModel || model<IUser>('UserModel', userSchema)
-export default UserModel
+const UserModel = models.UserModel || model<IUser>("UserModel", userSchema);
+export default UserModel;
