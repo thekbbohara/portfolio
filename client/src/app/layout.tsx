bdbb4e90b3024495b74e31f27dcc8458ac2e261e
thekbbohara <ytkbzcraft@gmail.com>
2024-12-18 18:06:19 +0545

    metadata

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,19 +1,65 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/nav/Footer";
 import { TwitterXFill, LinkedinSolid } from "@/assets/spfyicons";
 import Header from "@/components/nav/Header";
+const jetbrain = JetBrains_Mono({ subsets: ["latin"] });
 
-const jetbrain = JetBrains_Mono({ subsets: ["latin"] })
-
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  themeColor: "#011627",
+};
 export const metadata: Metadata = {
   title: "thekbbohara",
   description: "thekbbohara: fullstack developer portfolio website.",
-  keywords:
-    "fullstack developer, portfolio, web development, programming , thekbbohara, kbbohara, nextjs developer, golang developer, web developer",
-  category: "tsx",
+  keywords: [
+    "portfolio",
+    "thekbbohara",
+    "kbbohara",
+    "nepali fullstack developer",
+    "fullstack developer",
+    "web developer",
+    "mern developer",
+    "nextjs developer",
+    "remix developer",
+  ],
+  category: "Portfolio",
   creator: "thekbbohara",
+  metadataBase: new URL("https://kbbohara.com.np"),
+  openGraph: {
+    title: "thekbbohara | fullstack developer",
+    description:
+      "Explore the best portfolio of kb bohara. The best fullstack developer. Check out my work, skills, and contact information.",
+    images: [
+      {
+        url: "./public/assets/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Kb Bohara' portfolio website",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "thekbbohara | fullstack developer",
+    description:
+      "kbbohara.com.np - explore the best portfolio of kb bohara. The best fullstack developer. Check out my work, skills, and contact information.",
+    creator: "@thekbbohara",
+    site: "@thekbbohara",
+    images: [
+      {
+        url: "/assets/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "thekbbohara",
+      },
+    ],
+  },
+  alternates: {
+    canonical: "https:kbbohara.com.np",
+  },
 };
 
 export default function RootLayout({
@@ -47,7 +93,6 @@ export default function RootLayout({
                 icon: <LinkedinSolid className="size-5" />,
                 href: "https://linkedin.com/in/thekbbohara",
               },
-
             ]}
           />
         </main>
